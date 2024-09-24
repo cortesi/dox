7635859b05dd763837a952091438367f5beeb312
Aldo Cortesi <aldo@corte.si>
2024-09-24 12:11:12 +1200

    Helper macro to derive dox for primitive types.

--- crates/libdox/src/lib.rs
+++ crates/libdox/src/lib.rs
@@ -34,24 +34,40 @@ impl<T> Dox for Vec<T> {
     }
 }
 
-impl Dox for i32 {
-    fn dox() -> Field {
-        Field::Primitive(Primitive {
-            name: "i32".to_string(),
-            typ: "i32".to_string(),
-            doc: "32-bit signed integer".to_string(),
-        })
+macro_rules! impl_dox_for_primitive {
+    ($($t:ty, $name:expr, $doc:expr);*) => {
+        $(
+            impl Dox for $t {
+                fn dox() -> Field {
+                    Field::Primitive(Primitive {
+                        name: $name.to_string(),
+                        typ: stringify!($t).to_string(),
+                        doc: $doc.to_string(),
+                    })
+                }
+            }
+        )*
     }
 }
 
-impl Dox for String {
-    fn dox() -> Field {
-        Field::Primitive(Primitive {
-            name: "String".to_string(),
-            typ: "String".to_string(),
-            doc: "UTF-8 encoded, growable string".to_string(),
-        })
-    }
+impl_dox_for_primitive! {
+    i8, "i8", "8-bit signed integer";
+    i16, "i16", "16-bit signed integer";
+    i32, "i32", "32-bit signed integer";
+    i64, "i64", "64-bit signed integer";
+    i128, "i128", "128-bit signed integer";
+    isize, "isize", "Pointer-sized signed integer";
+    u8, "u8", "8-bit unsigned integer";
+    u16, "u16", "16-bit unsigned integer";
+    u32, "u32", "32-bit unsigned integer";
+    u64, "u64", "64-bit unsigned integer";
+    u128, "u128", "128-bit unsigned integer";
+    usize, "usize", "Pointer-sized unsigned integer";
+    f32, "f32", "32-bit floating point";
+    f64, "f64", "64-bit floating point";
+    bool, "bool", "Boolean type";
+    char, "char", "Unicode scalar value";
+    String, "String", "UTF-8 encoded, growable string"
 }
 
 pub trait Renderer {
